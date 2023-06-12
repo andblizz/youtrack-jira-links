77f8edf220759a50376c78223a2a280ca0f23e5a
Andrew Pochuritsin <mitchel_mit@mail.ru>
2023-06-12 12:51:03 +0300

    Added processing of tasks from the EDDEVORG project and lines from the error table.

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,23 @@
+// Определение констант
+const JIRA_BASE_URL = 'https://jira.mos.ru/browse/';
+const SHEET_BASE_URL = 'https://docs.google.com/spreadsheets/d/1EolYL1o0C0YQ8iMI-l2VjDILkU1ITBY8/edit#gid=1730455537&range=';
+
+// Функция для создания DOM элемента
+function createDOMElement(tag, props, ...children) {
+    const element = document.createElement(tag);
+    Object.assign(element, props);
+    children.forEach(child => {
+        if (typeof child === 'string') {
+            child = document.createTextNode(child);
+        }
+        element.appendChild(child);
+    });
+    return element;
+}
+
 function addJiraLink() {
   console.log("addJiraLink вызван");
+
   const labels = document.querySelectorAll('span.yt-issue-fields-panel__field-label');
 
   labels.forEach(label => {
@@ -7,77 +25,95 @@ function addJiraLink() {
 
     if (label.textContent.includes('Jira')) {
       console.log("Найдена метка Jira");
+
       const row = label.closest('tr');
       const valueElement = row.querySelector('.yt-issue-key-value-list__column_value');
       const valueText = valueElement.textContent.trim();
 
       // Создаем новую строку и ячейки для поля Jira links
-      const newRow = document.createElement('tr');
-      newRow.className = 'yt-issue-fields-panel__row';
-      const newKeyCell = document.createElement('td');
-      newKeyCell.className = 'yt-issue-key-value-list__column yt-issue-key-value-list__column_key';
-      const newValueCell = document.createElement('td');
-      newValueCell.className = 'yt-issue-key-value-list__column yt-issue-key-value-list__column_value';
+      const newRow = createDOMElement('tr', {className: 'yt-issue-fields-panel__row'});
+      const newKeyCell = createDOMElement('td', {className: 'yt-issue-key-value-list__column yt-issue-key-value-list__column_key'});
+      const newValueCell = createDOMElement('td', {className: 'yt-issue-key-value-list__column yt-issue-key-value-list__column_value'});
 
       // Добавляем метку поля Jira links
-      const newFieldLabel = document.createElement('span');
-      newFieldLabel.className = 'yt-issue-fields-panel__field-label';
-      newFieldLabel.textContent = 'Jira links';
+      const newFieldLabel = createDOMElement('span', {className: 'yt-issue-fields-panel__field-label', textContent: 'Jira links'});
       newKeyCell.appendChild(newFieldLabel);
 
       // Создаем элемент-контейнер для ссылок на задачи Jira
-      const jiraLinksContainer = document.createElement('span');
-      jiraLinksContainer.id = 'jiraLinksContainer';
+      const jiraLinksContainer = createDOMElement('span', {id: 'jiraLinksContainer'});
       newValueCell.appendChild(jiraLinksContainer);
 
       // Вставляем ячейки в новую строку
-      newRow.appendChild(newKeyCell);
-      newRow.appendChild(newValueCell);
+      newRow.append(newKeyCell, newValueCell);
 
       // Вставляем новую строку после строки с полем Jira
       row.parentNode.insertBefore(newRow, row.nextSibling);
 
-      // Функция для обновления ссылок на задачи Jira при загрузке страницы
+      // Функция для обновления ссылок на задачи Jira и Google Sheets при загрузке страницы
       function updateJiraLinks(valueText) {
-        // Разделение текста на массив номеров задач
         const taskNumbers = valueText.split(/[,/]\s*/).map(num => num.trim());
 
-        // Проверка на наличие номеров задачи и отсутствие текста
-        const containsOnlyNumbers = taskNumbers.every(num => /^\d+$/.test(num));
+        jiraLinksContainer.innerHTML = '';
 
-        if (containsOnlyNumbers) {
-          jiraLinksContainer.innerHTML = '';
-          taskNumbers.forEach((num, index) => {
-            const jiraLink = document.createElement('a');
-            jiraLink.href = `https://jira.mos.ru/browse/EDDEV-${num}`;
-            jiraLink.target = '_blank';
-            jiraLink.innerText = `EDDEV-${num}`;
-            jiraLink.style.color = '#70b1e6';
-            jiraLink.style.textDecoration = 'none';
-            jiraLinksContainer.appendChild(jiraLink);
-            if (index < taskNumbers.length - 1) {
-              const separator = valueText.includes('/') ? ' / ' : ', ';
-              jiraLinksContainer.appendChild(document.createTextNode(separator));
-            }
-          });
+      // Регулярные выражения и поиск номеров задач и строк
+      const numRegex = /^\d+$/;
+      const orgRegex = /^EDDEVORG-\d+$/;
+      const strRegex = /стр\. (\d+)(?:-(\d+))?/i;
+
+      taskNumbers.forEach((num, index) => {
+        let link;
+
+        if (numRegex.test(num)) {
+          link = createDOMElement('a', {href: `${JIRA_BASE_URL}EDDEV-${num}`, target: '_blank', textContent: `EDDEV-${num}`});
+        } else if (orgRegex.test(num)) {
+          link = createDOMElement('a', {href: `${JIRA_BASE_URL}${num}`, target: '_blank', textContent: num});
         } else {
-          newRow.style.display = 'none';
+          const match = strRegex.exec(num);
+          if (match) {
+            const rowStart = parseInt(match[1], 10) + 1;
+            const rowEnd = match[2] ? parseInt(match[2], 10) + 1 : rowStart;
+            link = createDOMElement('a', {href: `${SHEET_BASE_URL}${rowStart}:${rowEnd}`, target: '_blank', textContent: num});
+          }
         }
+
+        if (link) {
+          link.style.color = '#70b1e6';
+          link.style.textDecoration = 'none';
+          jiraLinksContainer.appendChild(link);
+
+          if (index < taskNumbers.length - 1) {
+            const separator = valueText.includes('/') ? ' / ' : ', ';
+            jiraLinksContainer.appendChild(document.createTextNode(separator));
+          }
+        }
+      });
+
+        // Показываем строку Jira links, если есть хотя бы одна ссылка
+        newRow.style.display = jiraLinksContainer.childNodes.length ? '' : 'none';
       }
 
-      // Обновляем ссылки при загрузке страницы
-      updateJiraLinks(valueText);
+      try {
+        // Обновляем ссылки при загрузке страницы
+        updateJiraLinks(valueText);
+      } catch (e) {
+        console.error('Ошибка при обновлении ссылок на Jira:', e);
+      }
     }
   });
 }
 
 function waitForYouTrackFields() {
   console.log("Ожидание полей Youtrack");
+
   const observer = new MutationObserver((mutations) => {
     if (document.querySelector('span.yt-issue-fields-panel__field-label')) {
       console.log("Поля Youtrack найдены");
       observer.disconnect();
-      addJiraLink();
+      try {
+        addJiraLink();
+      } catch (e) {
+        console.error('Ошибка при добавлении ссылок на Jira:', e);
+      }
     }
   });
 
